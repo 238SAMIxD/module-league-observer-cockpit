2f7e5b6e9bfe00cb88e112edf419a64938e90c1f
Samuel Jędrzejewski <samijedrzejewski@gmail.com>
2026-04-14 23:14:20 +0200

    a11y

--- frontend/cockpit/cockpit.js
+++ frontend/cockpit/cockpit.js
@@ -1,3 +1,10 @@
+document.addEventListener('keydown', (e) => {
+  if (e.target.getAttribute('role') === 'button' && (e.key === 'Enter' || e.key === ' ')) {
+    e.preventDefault();
+    e.target.click();
+  }
+});
+
 function getTeam() {
   const selectedTeam = document.querySelector("[name=team]:checked");
   return selectedTeam ? parseInt(selectedTeam.value, 10) : 100;
